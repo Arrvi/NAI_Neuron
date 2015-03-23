9cdece714d19a631e920682345cff84791c7fe6d
Arrvi <s11531@pjwstk.edu.pl>
2015-03-23 14:40:23 +0100

    Added learning form stem

--- src/pja/s11531/nai/NeuronTester.java
+++ src/pja/s11531/nai/NeuronTester.java
@@ -9,52 +9,67 @@ import java.math.BigDecimal;
 public class NeuronTester {
     private static JFrame frame;
     private JPanel mainPanel;
-    private JPanel       visualizationPane;
-    private JPanel       configPane;
-    private JTextField   input1;
-    private JTextField   input2;
-    private JTextField   input0;
+    private JPanel visualizationPane;
+    private JPanel configPane;
+    private JTextField input1;
+    private JTextField input2;
+    private JTextField input0;
     private JRadioButton unipolarRadioButton;
     private JRadioButton bipolarRadioButton;
-    private JComboBox    functionType;
-    private JButton      calculateButton;
+    private JComboBox functionType;
+    private JButton calculateButton;
     private JRadioButton upRadioButton;
     private JRadioButton downRadioButton;
-    
-    public NeuronTester () {
-        calculateButton.addActionListener( ( actionEvent ) -> calculate() );
+    private JTable learningSetsTable;
+    private JTextField learningSetCenter;
+    private JButton pointPickerButton;
+    private JTextField learningSetVariance;
+    private JTextField learningSetQuantity;
+    private JButton addLearningSet;
+    private JSlider learningFactor;
+    private JTextField learningEpochs;
+    private JButton learnButton;
+
+    public NeuronTester() {
+        calculateButton.addActionListener((actionEvent) -> calculate());
     }
-    
-    public static void main ( String[] args ) {
-        frame = new JFrame( "NeuronTester" );
-        frame.setContentPane( new NeuronTester().mainPanel );
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        frame = new JFrame("NeuronTester");
+        frame.setContentPane(new NeuronTester().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible( true );
-        frame.setResizable( false );
+        frame.setVisible(true);
+        frame.setResizable(false);
     }
-    
-    private void createUIComponents () {
+
+    private void createUIComponents() {
         visualizationPane = new VisualizationPanel();
     }
-    
-    private void calculate () {
+
+    private void calculate() {
         Neuron neuron;
         try {
             TransferFunction func = new StepTransferFunction(
-                    ( unipolarRadioButton.isSelected() ? StepTransferFunction.UNIPOLAR : StepTransferFunction.BIPOLAR ) |
-                            ( upRadioButton.isSelected() ? StepTransferFunction.CEIL : StepTransferFunction.FLOOR )
+                    (unipolarRadioButton.isSelected() ? StepTransferFunction.UNIPOLAR : StepTransferFunction.BIPOLAR) |
+                            (upRadioButton.isSelected() ? StepTransferFunction.CEIL : StepTransferFunction.FLOOR)
             );
-            neuron = new Neuron( new BigDecimal[] {
-                    new BigDecimal( input1.getText() ),
-                    new BigDecimal( input2.getText() ),
-                    new BigDecimal( input0.getText() )
-            }, func );
+            neuron = new Neuron(new BigDecimal[]{
+                    new BigDecimal(input1.getText()),
+                    new BigDecimal(input2.getText()),
+                    new BigDecimal(input0.getText())
+            }, func);
         } catch (NumberFormatException exc) {
-            JOptionPane.showMessageDialog( frame, "Wrong number format", "Number error", JOptionPane.ERROR_MESSAGE );
+            JOptionPane.showMessageDialog(frame, "Wrong number format", "Number error", JOptionPane.ERROR_MESSAGE);
             return;
         }
-    
-        ( (VisualizationPanel) visualizationPane ).setNeuron( neuron );
+
+        ((VisualizationPanel) visualizationPane).setNeuron(neuron);
     }
 }
